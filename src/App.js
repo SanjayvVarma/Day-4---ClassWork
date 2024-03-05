982b29ec0b69845e5020dea26b4601ccb85b1764
SanjayvVarma <spverma.sanjay56056@gmail.com>
2024-03-05 16:06:10 +0530

    geekfoods

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+ import Footer from './component/Footer';
+import { QuoteScreen } from './screen/QuoteScreen';
+
+
+function App() {
+  return (
+    <>
+      <QuoteScreen />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
